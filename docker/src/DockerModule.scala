2d5c5e8a88b5a4f847b9b6943daeaa9dcfe3e5c6
Victor Hugo Borja <vborja@apache.org>
2018-08-26 01:27:06 -0700

    dockerfile on context dir

--- docker/src/DockerModule.scala
+++ docker/src/DockerModule.scala
@@ -2,6 +2,8 @@ package mill.docker
 
 import mill._
 import ammonite.ops._
+import ImplicitWd._
+import pprint._
 
 trait DockerModule extends Module {
 
@@ -11,28 +13,24 @@ trait DockerModule extends Module {
 
   def dockerBaseImage: T[String] = "gcr.io/distroless/java:latest"
 
-  def dockerContent:T[String] = s"""
+  def dockerFile: T[String] = s"""
     |FROM ${dockerBaseImage()}
     |COPY app.jar /app.jar
     |ENTRYPOINT ["java", "-cp", "/app.jar", "${dockerMain()}"]
     |CMD []
     """.stripMargin
 
-  def dockerFile = T {
-    val dockerFile = T.ctx().dest / "Dockerfile"
-    write(dockerFile, dockerContent())
-    PathRef(dockerFile)
-  }
-
   def dockerBuild = T {
-    import ImplicitWd._
-    import pprint._
     val dest = T.ctx().dest
-    val docker:PathRef = dockerFile()
+
+    val file:PathRef = T.ctx().dest / "Dockerfile"
+    write(file, dockerFile())
+
     val singleJar:PathRef = assembly()
     cp(singleJar.path, dest/"app.jar")
-    %('docker, 'build, "-f", docker.path, "-t", dockerTag(), dest)
-    (singleJar, docker)
+
+    %('docker, 'build, "-f", file, "-t", dockerTag(), dest)
+    (singleJar, file)
   }
 
 }
